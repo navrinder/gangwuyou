7ed49a4aa1889dabe6aef6dae396232c5b87f7af
Taylor Brennan <taylor.j.brennan@gmail.com>
2015-06-06 22:09:46 -0700

    add knex database connection

---
+++ setupDatabase.js
@@ -0,0 +1,48 @@
+var knex = require('knex')({
+	client: 'mysql',
+	connection: {
+		host : config.database.host,
+		port : config.database.port,
+		user : config.database.user,
+		password : config.database.password,
+		database : config.database.database
+	}
+});
+
+// create users table
+knex.schema.hasTable('users').then(function(exists) {
+	if (!exists) {
+		knex.schema.createTable('users', function (table) {
+			table.increments('id');
+			table.string('user_name');
+			table.string('email_address');
+			table.string('password');
+			table.timestamps(); // adds dateTimes ceated_at and updated_at
+		});
+	}
+})
+
+// add test user
+.then(function() {
+	return knex.insert({
+		user_name: 'Test',
+		email_address: 'test@test.test',
+		password: 'password'
+	}).into('users');
+})
+
+// select test user
+.then(function() {
+	return knex('users')
+		.select('*');
+})
+
+// display results
+.map(function(row) {
+	console.log(row);
+})
+
+// error handler
+.catch(function(e) {
+	console.error(e);
+});
