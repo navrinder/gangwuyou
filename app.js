4516aeed8d00673c2626eef43724f4aefff7eb78
Taylor Brennan <taylor.j.brennan@gmail.com>
2015-06-22 00:48:07 -0700

    implement jwt

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var config = require('./config.json');
+var jwt = require('jwt-simple');
+var secret = config.secret;
 
+// http://knexjs.org/
+// SQL query builder
 var knex = require('knex')({
 	client: 'mysql',
 	connection: config.database
 });
 
-var admin = require('./lib/admin');
-var articles = require('./lib/articles');
-var feed = require('./lib/feed');
-var info = require('./lib/information');
-var questions = require('./lib/questions');
-
+// http://expressjs.com/
+// Web framework for nodejs
 var app = express();
 var router = express.Router();
 var port = process.env.PORT || 8080;
 
+// body parser module
+// parses url-encoded and json payloads
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// JSON web token verification for API usage.
+// Use config/generateToken.js to create API token.
+app.use(function (req, res, next) {
+	// token should be provided in Authorization header
+	console.log(req.headers)
+	var decoded = jwt.decode(req.headers.authorization, secret);
+	if (decoded && decoded.api) {
+		next();
+	} else {
+		res.status(401).send('Authorization failed');
+	}
+});
+
+// store knex connection in app object
 app.set('knex', knex);
 
-var users = require('./lib/users')(app);
+// models
+var admin = require('./lib/admin')(app);
+var answers = require('./lib/answers')(app);
+var articles = require('./lib/articles')(app);
+var info = require('./lib/information')(app);
 var login = require('./lib/login')(app);
+var questions = require('./lib/questions')(app);
+var users = require('./lib/users')(app);
 
 
 // routes
-router.use(function(req, res, next) {
-	console.log('alert');
-	next();
-});
+// router.use(function(req, res, next) {
+// 	console.log('alert');
+// 	next();
+// });
 
 router.get('/', function(req, res) {
 	res.json({ message: 'hello world' });
