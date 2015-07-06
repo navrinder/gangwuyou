0d138e1426521bc5a6fac4ffeade196fc5a31df2
Taylor Brennan <taylor.brennan@appdirect.com>
2015-07-05 22:33:04 -0700

    move routes to module

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,7 @@
 module.exports = function (app) {
 	return {
 		authUser : require('./authUser')(app),
-		authApi  : require('./authApi')(app)
+		authApi  : require('./authApi')(app),
+		routes   : require('./routes')
 	}
 };
