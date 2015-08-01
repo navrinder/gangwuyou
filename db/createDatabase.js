89a80a335f5c434f7b99021528d5dcd8ed7eec2a
Taylor Brennan <taylor.j.brennan@gmail.com>
2015-08-01 14:07:13 -0700

    add exists check to createDatabase

--- db/createDatabase.js
+++ db/createDatabase.js
@@ -9,43 +9,50 @@ var Promise = require('bluebird');
 var _ = require('lodash');
 
 function createTable(tableName) {
-  return knex.schema.createTable(tableName, function (table) {
-    var column;
-    var columnKeys = _.keys(Schema[tableName]);
-    _.each(columnKeys, function (key) {
-      if (Schema[tableName][key].type === 'text' && Schema[tableName][key].hasOwnProperty('fieldtype')) {
-        column = table[Schema[tableName][key].type](key, Schema[tableName][key].fieldtype);
-      }
-      else if (Schema[tableName][key].type === 'string' && Schema[tableName][key].hasOwnProperty('maxlength')) {
-        column = table[Schema[tableName][key].type](key, Schema[tableName][key].maxlength);
-      }
-      else {
-        column = table[Schema[tableName][key].type](key);
-      }
-      if (Schema[tableName][key].hasOwnProperty('nullable') && Schema[tableName][key].nullable === true) {
-        column.nullable();
-      }
-      else {
-        column.notNullable();
-      }
-      if (Schema[tableName][key].hasOwnProperty('primary') && Schema[tableName][key].primary === true) {
-        column.primary();
-      }
-      if (Schema[tableName][key].hasOwnProperty('unique') && Schema[tableName][key].unique) {
-        column.unique();
-      }
-      if (Schema[tableName][key].hasOwnProperty('unsigned') && Schema[tableName][key].unsigned) {
-        column.unsigned();
-      }
-      if (Schema[tableName][key].hasOwnProperty('references')) {
-        column.references(Schema[tableName][key].references);
-      }
-      if (Schema[tableName][key].hasOwnProperty('defaultTo')) {
-        column.defaultTo(Schema[tableName][key].defaultTo);
-      }
-    });
+  return knex.schema.hasTable(tableName).then(function(exists) {
+    if (!exists) {
+      console.log('Creating ' + tableName);
+      return knex.schema.createTable(tableName, function (table) {
+        var column;
+        var columnKeys = _.keys(Schema[tableName]);
+        _.each(columnKeys, function (key) {
+          if (Schema[tableName][key].type === 'text' && Schema[tableName][key].hasOwnProperty('fieldtype')) {
+            column = table[Schema[tableName][key].type](key, Schema[tableName][key].fieldtype);
+          }
+          else if (Schema[tableName][key].type === 'string' && Schema[tableName][key].hasOwnProperty('maxlength')) {
+            column = table[Schema[tableName][key].type](key, Schema[tableName][key].maxlength);
+          }
+          else {
+            column = table[Schema[tableName][key].type](key);
+          }
+          if (Schema[tableName][key].hasOwnProperty('nullable') && Schema[tableName][key].nullable === true) {
+            column.nullable();
+          }
+          else {
+            column.notNullable();
+          }
+          if (Schema[tableName][key].hasOwnProperty('primary') && Schema[tableName][key].primary === true) {
+            column.primary();
+          }
+          if (Schema[tableName][key].hasOwnProperty('unique') && Schema[tableName][key].unique) {
+            column.unique();
+          }
+          if (Schema[tableName][key].hasOwnProperty('unsigned') && Schema[tableName][key].unsigned) {
+            column.unsigned();
+          }
+          if (Schema[tableName][key].hasOwnProperty('references')) {
+            column.references(Schema[tableName][key].references);
+          }
+          if (Schema[tableName][key].hasOwnProperty('defaultTo')) {
+            column.defaultTo(Schema[tableName][key].defaultTo);
+          }
+        });
+      });
+    } else {
+      console.log(tableName + ' exists');
+    }
   });
-}
+};
 
 function createTables () {
   var tables = [];
